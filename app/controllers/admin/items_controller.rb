a696275fa5741446c56798a2d9b5eba5f1fd90e4
Sai-20 <ec2-user@ip-172-31-36-46.ec2.internal>
2022-09-03 04:05:49 +0000

    3.1.4商品新規登録END

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,20 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def show
+     @item = Item.find(params[:id])
+  end
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_items_path
   end
 
   def edit
