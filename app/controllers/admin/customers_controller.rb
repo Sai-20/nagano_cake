0cd9855d77137d4e7d87179d6b09b92150498e97
Sai-20 <ec2-user@ip-172-31-36-46.ec2.internal>
2022-09-04 17:28:51 +0000

    3.1.8会員一覧画面　update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,13 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customers = Customer.find(params[:id])
   end
 
   def edit
+    
   end
 end
