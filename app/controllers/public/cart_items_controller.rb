3b4a85bbae7d09cba7c069dbf7cb25f1470467f3
Sai-20 <pc.23.32ak@gmail.com>
2022-10-07 20:04:31 +0000

    2022/10/8

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -21,15 +21,25 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    current_customer.cart_items.find(params[:cart_item][:item_id])
+    if cart_item.update(cart_items_params)
     redirect_to public_cart_items_path
+    else
+      render 'public/items/show'
+    end
   end
 
   def destroy
+    cart_item_params.find_by(item_id: params[:cart_item][:item_id])
+    cart_item.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to public_cart_items_path
   end
 
+
+
   def destroy_all
-    @destroy_all = current_customer.destroy_all
+    @destroy_all = current_customer.cart_item.destroy_all
     redirect_to public_cart_items_path
   end
 
